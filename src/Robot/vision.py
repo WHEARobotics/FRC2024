b2201123f4dd6433acdb76bf81eca0e60302be7d
zach-B <zach.bibikov@whea.net>
2024-01-17 16:31:41 -1000

    vision file for robot code make sure wpilib is not imported because it will mess with the robot.py file

---
+++ src/Robot/vision.py
@@ -0,0 +1,12 @@
+
+import math
+import cscore
+
+import time # Temporary for diagnostics
+
+class Vision:
+                                                     
+    
+    def __init__(self) -> None:
+        pass
+
