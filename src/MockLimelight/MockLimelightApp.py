07bbfb087e9d1efd7e774f3262590c0eeb5d589d
Larry O'Brien <lobrien@knowing.net>
2024-01-23 09:05:00 -1000

    Draw robot around transform. Easier for rotations.

--- src/MockLimelight/MockLimelightApp.py
+++ src/MockLimelight/MockLimelightApp.py
@@ -16,29 +16,45 @@ class RobotWidget(QWidget):
         # It's not negative because widget coordinates are in "typewriter" space (0,0) is top left, down is +y
         self.origin_offset_y = 4 * pixels_per_meter
         self.pixels_per_meter = pixels_per_meter
+        self.field_rotation = 0
 
-    # TODO: I'm not thrilled about allocating a new QPainter every time we paint. But it seems like
-    # when you put the QPainter in the init function, it leads to trouble. So maybe QPainter is a
-    # very light "current drawing" context and it's okay to allocate a new one every time we paint?
-    # Still, maybe we should at least try to push the QPen and QColor into the init function.
+    # Called by Qt when the widget needs to be redrawn (e.g. when it's first shown, or when update() is called)
     def paintEvent(self, event):
         q = QPainter(self)
         q.setPen(QPen(Qt.black, 5, Qt.SolidLine))
         q.setBrush(QColor(255, 0, 0, 127))
-        # Screen position is field position * pixels per meter + the origin offset - half the robot size
-        screen_x = int(self.field_pos[0] * self.pixels_per_meter + self.origin_offset_x - self.robot_edge_px/2)
-        screen_y = int(self.field_pos[1] * self.pixels_per_meter + self.origin_offset_y - self.robot_edge_px/2)
 
-        q.drawRect(screen_x, screen_y, self.robot_edge_px, self.robot_edge_px)
+        # Transform to center of robot
+        transform = q.transform()
+        # Set origin to the center of the robot
+        transform.translate(self.field_pos[0] * self.pixels_per_meter + self.origin_offset_x,
+                            self.field_pos[1] * self.pixels_per_meter + self.origin_offset_y)
+        # Rotate around center of robot
+        transform.rotate(self.field_rotation)
+        # Use this new translated and rotated transform for drawing
+        q.setTransform(transform)
+        # Note that at this point, the origin is at the center of the robot and the robot is rotated
+        # So we can draw the robot as if it's at (0,0) and it will appear on the screen properly located and rotated
+        q.drawRect(int(-self.robot_edge_px/2),
+                   int(-self.robot_edge_px/2),
+                   int(self.robot_edge_px),
+                   int(self.robot_edge_px))
+        # Draw directional arrow
+        q.setPen(QPen(Qt.black, 2, Qt.SolidLine))
+        arrow_start = (0, 0)
+        arrow_end = (self.robot_edge_px + 5, 0)
+        q.drawLine(int(arrow_start[0]), int(arrow_start[1]), int(arrow_end[0]), int(arrow_end[1]))
+        q.drawLine(int(arrow_end[0]), int(arrow_end[1]), int(arrow_end[0] - 10), int(arrow_end[1] - 10))
+        q.drawLine(int(arrow_end[0]), int(arrow_end[1]), int(arrow_end[0] - 10), int(arrow_end[1] + 10))
+
+        # Anything drawn after this call to `end` will be in the original coordinate system
         q.end()
 
-    def update_position(self, field_x, field_y):
-        # TODO: Field X should be in "Field Space" (0,0) is the center of the field. Conversion to screen pos
-        #  is display specific.
+    def update_position(self, field_x, field_y, field_rotation):
+        # "Field Space" (0,0) is the center of the field in meters.
         self.field_pos = (field_x, field_y)
+        self.field_rotation = field_rotation
         self.update()
-
-
 
 
 class MainWindow(QMainWindow):
@@ -63,7 +79,6 @@ class MainWindow(QMainWindow):
         control_panel = QWidget()
         control_panel_layout = QVBoxLayout(control_panel)
 
-
         # Sliders
         self.field_x_slider = QSlider(Qt.Horizontal)
         self.field_x_slider.setMinimum(0)
@@ -73,8 +88,17 @@ class MainWindow(QMainWindow):
         self.field_y_slider = QSlider(Qt.Horizontal)
         self.field_y_slider.setMinimum(-400)
         self.field_y_slider.setMaximum(400)
+        self.field_y_slider.setValue(0)
         self.field_y_slider.valueChanged.connect(self.update_robot_pos)
         control_panel_layout.addWidget(self.field_y_slider)
+
+        self.field_rotation_slider = QSlider(Qt.Horizontal)
+        self.field_rotation_slider.setMaximum(-180)
+        self.field_rotation_slider.setMaximum(180)
+        self.field_rotation_slider.setValue(0)
+        self.field_rotation_slider.valueChanged.connect(self.update_robot_pos)
+        control_panel_layout.addWidget(self.field_rotation_slider)
+
         # Mock Limelight Radio Button
         self.mock_limelight = QRadioButton("Mock Limelight")
         self.mock_limelight.setChecked(False)  # Default to False
@@ -91,14 +115,15 @@ class MainWindow(QMainWindow):
 
         return control_panel
 
-    def _build_field(self):
+    @staticmethod
+    def _build_field():
         # Background SVG
         svg_widget = QSvgWidget('./field_right.svg')
-        widget_width = 800 # pixels
-        field_size_x = 9 # meters
+        widget_width = 800  # pixels
+        field_size_x = 9  # meters
         field_size_y = 8
-        pixels_per_meter = int(widget_width / field_size_x) # 1px == 1cm
-        svg_widget.setFixedSize(field_size_x * pixels_per_meter, field_size_y *  pixels_per_meter)
+        pixels_per_meter = int(widget_width / field_size_x)  # 1px == 1cm
+        svg_widget.setFixedSize(field_size_x * pixels_per_meter, field_size_y * pixels_per_meter)
         # Place it at 0,0
         svg_widget.move(0, 0)
 
@@ -108,15 +133,13 @@ class MainWindow(QMainWindow):
         robot = RobotWidget(pixels_per_meter)
         robot.setParent(svg_widget)
 
-
-
         return svg_widget, robot
 
     def update_robot_pos(self):
         field_x = self.field_x_slider.value() / 100
         field_y = self.field_y_slider.value() / 100
-        self.robot.update_position(field_x, field_y)
-
+        field_rotation = self.field_rotation_slider.value()
+        self.robot.update_position(field_x, field_y, field_rotation)
 
 
 app = QApplication(sys.argv)
