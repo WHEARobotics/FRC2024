e62c309df630c2b433d24f274793ec0f8cc00ab8
zach-B <zach.bibikov@whea.net>
2024-01-25 11:38:56 -1000

    current swerve code not working just updating to use another computer

--- src/Robot/robot.py
+++ src/Robot/robot.py
@@ -8,7 +8,7 @@ from wpimath.kinematics import SwerveModulePosition, SwerveModuleState
 import time
 
 from vision import Vision #Vision file import
-from CrescendoSwerveDrivetrain import CrescendoSwerveDrivetrain
+# from CrescendoSwerveDrivetrain import CrescendoSwerveDrivetrain
 from CrescendoSwerveModule import CrescendoSwerveModule
 
 class Myrobot(wpilib.TimedRobot):
@@ -16,12 +16,12 @@ class Myrobot(wpilib.TimedRobot):
  
 
     def robotInit(self):
-        wpilib.CameraServer.launch()
+        # wpilib.CameraServer.launch()
 
         self.xbox = wpilib.XboxController(0)
-        #self.swerve = CrescendoSwerveModule
+        # self.swerve = CrescendoSwerveModule
 
-        self.frontLeft = CrescendoSwerveModule(3, 2, 1, 0)
+        self.frontLeft = CrescendoSwerveModule(2, 3, 0, 0)
 
           #changes the limit of rate of change in the input value. the smaller the number the longer it takes to reach the destination if slew rate = 1 and input = 1 it would take 1 secnd to accelerate to full speed
         #if input = 0.5 it would take 0.5 seconds to accelerate to desired speed.
@@ -71,36 +71,38 @@ class Myrobot(wpilib.TimedRobot):
         elif self.xbox.getLeftTriggerAxis() > 0.9 and self.xbox.getAButton():
             self.halfSpeed = False
 
-        if self.halfSpeed == True:
-            joystick_y = self.swerve.joystickscaling(self.joystick_y )
-            xSpeed = self.xSpeedLimiter.calculate(joystick_y) * self.swerve.getMaxSpeed() / 6
+        #1/22/2024 commented out whats below for more simplification, we dont need joystickscaling maxspeed etc.
 
-            joystick_x = self.swerve.joystickscaling(self.joystick_x )
-            ySpeed = self.ySpeedLimiter.calculate(joystick_x) * self.swerve.MAX_SPEED / 6
+        # if self.halfSpeed == True:
+        #     joystick_y = self.swerve.joystickscaling(self.joystick_y )
+        #     xSpeed = self.xSpeedLimiter.calculate(joystick_y) * self.swerve.getMaxSpeed() / 6
 
-            rot = self.swerve.joystickscaling(rot)
-            rot = self.rotLimiter.calculate(rot) * self.swerve.MAX_ANGULAR_SPEED / 3
+        #     joystick_x = self.swerve.joystickscaling(self.joystick_x )
+        #     ySpeed = self.ySpeedLimiter.calculate(joystick_x) * self.swerve.MAX_SPEED / 6
+
+        #     rot = self.swerve.joystickscaling(rot)
+        #     rot = self.rotLimiter.calculate(rot) * self.swerve.MAX_ANGULAR_SPEED / 3
+
+        #     # self.swerve.drive(xSpeed, ySpeed, rot, fieldRelativeParam)
+
+        # else:
+        #     # Get the x speed. We are inverting this because Xbox controllers return
+        #     # negative values when we push forward.
+        #     joystick_y = self.swerve.joystickscaling(self.joystick_y )
+        #     xSpeed = self.xSpeedLimiter.calculate(joystick_y) * self.swerve.getMaxSpeed()
+
+        #     # Get the y speed. We are inverting this because Xbox controllers return
+        #     # negative values when we push to the left.
+        #     joystick_x = self.swerve.joystickscaling(self.joystick_x)
+        #     ySpeed = self.ySpeedLimiter.calculate(joystick_x) * self.swerve.MAX_SPEED
+
+        #     rot = self.swerve.joystickscaling(rot)
+        #     rot = self.rotLimiter.calculate(rot) * self.swerve.MAX_ANGULAR_SPEED
 
             # self.swerve.drive(xSpeed, ySpeed, rot, fieldRelativeParam)
-
-        else:
-            # Get the x speed. We are inverting this because Xbox controllers return
-            # negative values when we push forward.
-            joystick_y = self.swerve.joystickscaling(self.joystick_y )
-            xSpeed = self.xSpeedLimiter.calculate(joystick_y) * self.swerve.getMaxSpeed()
-
-            # Get the y speed. We are inverting this because Xbox controllers return
-            # negative values when we push to the left.
-            joystick_x = self.swerve.joystickscaling(self.joystick_x)
-            ySpeed = self.ySpeedLimiter.calculate(joystick_x) * self.swerve.MAX_SPEED
-
-            rot = self.swerve.joystickscaling(rot)
-            rot = self.rotLimiter.calculate(rot) * self.swerve.MAX_ANGULAR_SPEED
-
-            # self.swerve.drive(xSpeed, ySpeed, rot, fieldRelativeParam)
-            self.angle = Rotation2d(self.xbox.getLeftX(), self.xbox.getLeftY())
-            self.state = SwerveModuleState(0.1, self.angle)
-            self.frontLeft.setDesiredState(self.state, True)
+        self.angle = Rotation2d(self.xbox.getLeftX(), self.xbox.getLeftY())
+        self.state = SwerveModuleState(0.1, self.angle)
+        self.frontLeft.setDesiredState(self.state, True)
     def teleopExit(self):
         pass
     
