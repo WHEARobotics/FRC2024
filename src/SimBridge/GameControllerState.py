86714f86fb20416c5321e1f4d2ca43471700430f
Larry O'Brien <lobrien@knowing.net>
2024-01-30 17:25:22 -1000

    Control string is whitespace sensitive. X controls shooter.

--- src/SimBridge/GameControllerState.py
+++ src/SimBridge/GameControllerState.py
@@ -22,28 +22,31 @@ class GameControllerState:
     trigger_l: float
     trigger_r: float
 
-    def to_xrc_control_strings(self) -> str:
-        
-        control_string = "// Written by SimBridge\n"
-        control_string += f"a= {1 if self.a else 0}\n"
-        control_string += f"b= {1 if self.b else 0}\n"
-        control_string += f"x= {1 if self.x else 0}\n"
-        control_string += f"y= {1 if self.y else 0}\n"
-        control_string += f"dpad_down= {1 if self.dpad_down else 0}\n"
-        control_string += f"dpad_up= {1 if self.dpad_up else 0}\n"
-        control_string += f"dpad_left= {1 if self.dpad_left else 0}\n"
-        control_string += f"dpad_right= {1 if self.dpad_right else 0}\n"
-        control_string += f"bumper_l= {1 if self.bumper_l else 0}\n"
-        control_string += f"bumper_r= {1 if self.bumper_r else 0}\n"
-        control_string += f"stop= {1 if self.stop else 0}\n"
-        control_string += f"restart= {1 if self.restart else 0}\n"
+    flywheel_throttle : float = 1.0
 
-        control_string += f"right_y= {self.right_y:.3f}\n"
-        control_string += f"right_x= {self.right_x:.3f}\n"
-        control_string += f"left_y= {self.left_y:.3f}\n"
-        control_string += f"left_x= {self.left_x:.3f}\n"
-        control_string += f"trigger_l= {self.trigger_l:.3f}\n"
-        control_string += f"trigger_r= {self.trigger_r:.3f}\n"
+    def to_xrc_control_strings(self) -> str:
+        # Note: Sensitive to whitespace! No spaces between = and value
+        control_string = "// Written by SimBridge\n"
+        control_string += f"a={1.0 if self.a else 0.0}\n"
+        control_string += f"b={1.0 if self.b else 0.0}\n"
+        control_string += f"x={1.0 if self.x else 0.0}\n"
+        control_string += f"y={1.0 if self.y else 0.0}\n"
+        control_string += f"dpad_down={1 if self.dpad_down else 0}\n"
+        control_string += f"dpad_up={1 if self.dpad_up else 0}\n"
+        control_string += f"dpad_left={1 if self.dpad_left else 0}\n"
+        control_string += f"dpad_right={1 if self.dpad_right else 0}\n"
+        control_string += f"bumper_l={1 if self.bumper_l else 0}\n"
+        control_string += f"bumper_r={1 if self.bumper_r else 0}\n"
+        control_string += f"stop={1 if self.stop else 0}\n"
+        control_string += f"restart={1 if self.restart else 0}\n"
+
+        control_string += f"right_y={self.right_y:.3f}\n"
+        control_string += f"right_x={self.right_x:.3f}\n"
+        control_string += f"left_y={self.left_y:.3f}\n"
+        control_string += f"left_x={self.left_x:.3f}\n"
+        control_string += f"trigger_l={self.trigger_l:.3f}\n"
+        control_string += f"trigger_r={self.trigger_r:.3f}\n"
+
 
         return control_string
 
