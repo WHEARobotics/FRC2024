cc0553a59d26c978f88a56b226459cf2697dc021
Larry O'Brien <lobrien@knowing.net>
2024-03-19 08:30:34 -1000

    How to set widget properties

--- src/helloworld/shuffleboard/robot.py
+++ src/helloworld/shuffleboard/robot.py
@@ -1,10 +1,13 @@
+import ntcore
 import wpilib
 from wpilib import SendableChooser
 from wpilib.shuffleboard import Shuffleboard
 from wpilib.shuffleboard import BuiltInWidgets
+from ntcore._ntcore import Value as ntV
+
 
 class ShuffleTab:
-    def __init__(self, tab_name):
+    def __init__(self, tab_name : str):
         self.tab = Shuffleboard.getTab(tab_name)
 
         # Read-only value showing some information
@@ -17,11 +20,22 @@ class ShuffleTab:
                           )
 
         #  Read-write value showing the distance to the target
+        ## To set properties, you need to use the ntcore._ntcore.Value.make{type} methods, for instance:
+        ## ntV.makeDouble(0) or ntV.makeString("green")
+        ## To find the available properties, see https://robotpy.readthedocs.io/projects/robotpy/en/2024.1.1.1/wpilib.shuffleboard/BuiltInWidgets.html
+        ## You then need to call `withProperties` on the entry to set the properties
+        distance_properties = {
+            "min": ntV.makeDouble(0),
+            "max": ntV.makeDouble(15),
+            "Block increment": ntV.makeDouble(1)
+        }
+
         self.distance_entry = (self.tab
                                 .add("Distance to target", -1)
                                 .withWidget(BuiltInWidgets.kNumberBar)
                                 .withSize(3, 1)
                                 .withPosition(1, 2)
+                                .withProperties(distance_properties)
                                 .getEntry()
                               )
 
@@ -30,6 +44,7 @@ class ShuffleTab:
                                         .add("Activate Climb Lock", True)
                                         .withWidget(BuiltInWidgets.kToggleButton)
                                         .withSize(2, 1)
+                                        .withProperties({"color When True": ntV.makeString("green"), "color When False": ntV.makeString("red")})
                                         .getEntry()
                                         )
 
@@ -39,6 +54,7 @@ class ShuffleTab:
             .addPersistent("Persistent Speed", 3.0)
             .withWidget(BuiltInWidgets.kNumberSlider)
             .withSize(4, 1)
+            .withProperties({"min": ntV.makeDouble(0), "max": ntV.makeDouble(5)})
             .getEntry()
         )
 
@@ -60,7 +76,12 @@ class ShuffleBoardReadWriteBot(wpilib.TimedRobot):
         solenoidEnabled = self.shuffle_tab.activate_climb_solenoid.get().getBoolean()
         print(f"Solenoid enabled {solenoidEnabled}")
 
+from robotpy.main import main
+import sys
+
 if __name__ == '__main__':
-    wpilib.run(ShuffleBoardReadWriteBot)
+    sys.argv[0] = 'robotpy sim'
+    main()
+    #wpilib.run(ShuffleBoardReadWriteBot)
 
 
